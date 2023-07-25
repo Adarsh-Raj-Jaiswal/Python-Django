eb6c9f9d38e0e2e8db9f76ca0e4a91d12f3f400d
Adarsh Raj Jaiswal <adarshraj3309@gmail.com>
2023-07-25 23:58:36 +0530

    pushing my code :)

---
+++ emp_app/admin.py
@@ -0,0 +1,9 @@
+import imp
+from django.contrib import admin
+
+# Register your models here.
+from .models import Employee,Role,Department
+
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Department)
